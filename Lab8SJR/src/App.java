05c35f1d9bcf2d2509943402864f59f2932b4043
Sam <sreynebeau11@gmail.com>
2025-04-09 15:19:13 -0500

    computing the x value should be good now. just have to do the unidollars, and then figure out the draw method and it should be good

--- Lab8SJR/src/App.java
+++ Lab8SJR/src/App.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 import lab8.Unicorns;
 
@@ -14,6 +15,26 @@ public class App {
 
         //create and intialize object with # unicorns and iterations from user input
         Unicorns unicorns = new Unicorns(numUnicrons, numIterations);
+
+        unicorns.compute();
+
+        unicorns.print();
+
+        // Random random = new Random(42);
+        // // Generate random double between -1e8 and 1e8
+        // double randomDouble = (random.nextDouble() * 2e8) - 1e8;
+
+        // // Round to nearest 0.1
+        // randomDouble = Math.round(randomDouble * 10.0) / 10.0;
+
+        // Print the result, ensuring it's displayed in decimal format
+        //System.out.printf("%.1f%n", randomDouble);  // Format to 1 decimal place
+
+
+        //System.out.println(randomDouble);
+        //System.out.println(anotherOne);
+
+        //System.out.println(string);
 
         //object.numUnicorns object.numIterations
         
--- Lab8SJR/src/lab8/Unicorns.java
+++ Lab8SJR/src/lab8/Unicorns.java
@@ -1,6 +1,8 @@
 package lab8;
 import net.datastructures.SortedTableMap;
-import net.datastructures.ArrayList;
+import java.util.Random;
+
+//import net.datastructures.ArrayList;
 
 /**
  * Name: Sam Reynebeau
@@ -9,7 +11,7 @@ import net.datastructures.ArrayList;
  * Bugs: None known
  * Reflection: stuff and things and stuff
  */
-public class Unicorns {
+public class Unicorns{
 
     //Nested Unicorn class----------------------------------
     public class Unicorn{
@@ -26,28 +28,37 @@ public class Unicorns {
 
 
     //sorted map to keep track of each unicorn, it's x-value and # of unidollars
-    SortedTableMap<Integer, Unicorn> map = new SortedTableMap<>();
+    public SortedTableMap<Integer, Unicorn> map = new SortedTableMap<>();
 
     //these will be set from user in main method when object is created
     private int numUnicorns = 0;
     private int numIterations = 0;
+    //create random seed so each time random is called it starts from the same place and will be reproducible
+    private Random random = new Random(42);
 
 
     public Unicorns(int numUnicorns, int numIterations){
+        //set instance variables
         this.numUnicorns = numUnicorns;
         this.numIterations = numIterations;
 
-        //initUnicorns()?
+        //create Unicorns and store them
+        initUnicornsInMap();
     }
 
     
 
 
-
-    public void initUnicorns(){
-        //iterate thru numUnicorns and initialize that many unicorn objects
+    /**
+     * This method creates each unicorn object dpending on numUnicorns variable passed in by user
+     * and also stores them in the sorted map
+     */
+    public void initUnicornsInMap(){
+        //iterate thru numUnicorns and initialize that many unicorn objects into map
         for(int i = 0; i < numUnicorns; i++){
 
+            Unicorn unicorn = new Unicorn();
+            map.put(i,unicorn);
 
         }
 
@@ -56,10 +67,80 @@ public class Unicorns {
         //create a new variable every time.. which nah that sounds stupid. 
     }
 
-    public void initializeMap(){
+    public void runUnicornSimulation(){
+        compute();
+        print();
+        
 
     }
+
+
+
+    /**
+     * computes things
+     */
+    public void compute(){
+
+        //iterate thru and set each unicorn's values numIterations times
+        for(int i = 0; i < numIterations; i++)
+            //set random x coordinate for each unicorn
+            for(int j = 0; j < numUnicorns; j++){
+
+                // Generate random double between -1e8 and 1e8
+                double randomDouble = (random.nextDouble() * 2e8) - 1e8;
+                // Round to nearest 0.1
+                randomDouble = Math.round(randomDouble * 10.0) / 10.0;
+
+                //get unicorn at key j and change it's x value to random
+                Unicorn unicorn = map.get(j);
+                unicorn.xCoordinate = randomDouble;
+
+                //reset unicorn j to changed unicorn
+                map.put(j,unicorn);
+            }
+
+
+
+    }
+
+    /**
+     * TODO:
+     */
+    public void print(){
+        String unicorn = "Unicorn";
+        String xi = "xi";
+        String ui = "ui";
+        
+        //System.out.print(map.get(1).xCoordinate);
+        int unicornWidth = unicorn.length();
+
+        System.out.printf(unicorn + "%20s %15s", xi, ui);
+        System.out.println();
+
+
+        //loop through unicorns
+        for(int i = 0; i < numUnicorns; i++){
+
+            Unicorn unicornValues = map.get(i);
+            double xValue = unicornValues.xCoordinate;
+            double uniDollars = unicornValues.uniDollars;
+
+
+            System.out.printf("%" + unicornWidth + "d" + "%" + 20 + "f" + "%" + 16 +"f", i, xValue, uniDollars);
+            System.out.println();
+        
+        }
+
+
+    }
+
+
+    public void draw(){
+
+    }
+
     //methods and such
+        //everything()
 
         //initialize map
 
@@ -68,8 +149,5 @@ public class Unicorns {
         //print
 
         //draw()
-
-
-
 
 }
