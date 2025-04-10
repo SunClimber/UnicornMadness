7fefccfa2f1efc7a7f807c92e4c42f29677dc442
Sam <sreynebeau11@gmail.com>
2025-04-09 21:07:35 -0500

    pretty much refactored everything... created 2 maps instead of the 1, and created an ArrayList to store the Unicorn objects in

--- Lab8SJR/src/lab8/Unicorns.java
+++ Lab8SJR/src/lab8/Unicorns.java
@@ -1,8 +1,7 @@
 package lab8;
 import net.datastructures.SortedTableMap;
 import java.util.Random;
-
-//import net.datastructures.ArrayList;
+import net.datastructures.ArrayList;
 
 /**
  * Name: Sam Reynebeau
@@ -18,6 +17,7 @@ public class Unicorns{
         //Every unicorn starts with 
         double uniDollars = 1000000.0;
         double xCoordinate = 0.0;
+        int unicornID = 0;
 
         public Unicorn(){
 
@@ -27,8 +27,18 @@ public class Unicorns{
     //--------------END OF NESTED CLASS---------------------
 
 
-    //sorted map to keep track of each unicorn, it's x-value and # of unidollars
-    public SortedTableMap<Integer, Unicorn> map = new SortedTableMap<>();
+    //sorted map where the x-coordinate is the key
+    public SortedTableMap<Double, Unicorn> xSortedMap = new SortedTableMap<>();
+
+    //sorted map where the unidollars is the key
+    public SortedTableMap<Double, Unicorn> uSortedMap = new SortedTableMap<>();
+
+    //this list is needed to iterate through unicorns by ID because xi and ui are not known until sim runs
+    //all unicorns will be stored in here.
+    public ArrayList<Unicorn> unicornList = new ArrayList<>();
+
+
+
 
     //these will be set from user in main method when object is created
     private int numUnicorns = 0;
@@ -54,19 +64,24 @@ public class Unicorns{
      * and also stores them in the sorted map
      */
     public void initUnicornsInMap(){
-        //iterate thru numUnicorns and initialize that many unicorn objects into map
+        //iterate thru numUnicorns, initalize both maps, and store id for each unicorn
         for(int i = 0; i < numUnicorns; i++){
 
             Unicorn unicorn = new Unicorn();
-            map.put(i,unicorn);
+            unicorn.unicornID = i;
+            xSortedMap.put(unicorn.xCoordinate,unicorn);
+
+            uSortedMap.put(unicorn.uniDollars,unicorn);
+
+            //add to list
+            unicornList.add(i,unicorn);
 
         }
-
-        //have to initialize map at same time I think, cuz you have to store each 
-        //unicorn as it's created otherwise you'll iterate over it unless you want to
-        //create a new variable every time.. which nah that sounds stupid. 
     }
-
+    
+    /**
+     * run this biotch
+     */
     public void runUnicornSimulation(){
         compute();
         print();
@@ -81,22 +96,35 @@ public class Unicorns{
      */
     public void compute(){
 
-        //iterate thru and set each unicorn's values numIterations times
+        //iterate thru and re-generate each unicorn's values numIterations times
         for(int i = 0; i < numIterations; i++)
             //set random x coordinate for each unicorn
             for(int j = 0; j < numUnicorns; j++){
 
-                // Generate random double between -1e8 and 1e8
-                double randomDouble = (random.nextDouble() * 2e8) - 1e8;
-                // Round to nearest 0.1
-                randomDouble = Math.round(randomDouble * 10.0) / 10.0;
+                // Generate random double between -1 and 1
+                double randomDouble = (random.nextDouble(-1,1));
 
+
+                //get unicorn at specified index
+                Unicorn realUnicorn = unicornList.get(i);
+
+
+                //TODO: figure out how to compute the new unidollar value
+
+
+
+                
                 //get unicorn at key j and change it's x value to random
-                Unicorn unicorn = map.get(j);
-                unicorn.xCoordinate = randomDouble;
+                //Unicorn unicorn = xSortedMap.get(j);
+                double newXvalue = realUnicorn.xCoordinate + (randomDouble * realUnicorn.uniDollars);
+                // Round to nearest 0.1
+                newXvalue = Math.round(newXvalue * 10.0) / 10.0;
+                realUnicorn.xCoordinate = newXvalue;
 
-                //reset unicorn j to changed unicorn
-                map.put(j,unicorn);
+                //reset unicorn of that xCoordinate to changed unicorn
+                xSortedMap.put(realUnicorn.xCoordinate,realUnicorn);
+                //uSortedMap.put
+
             }
 
 
@@ -121,12 +149,16 @@ public class Unicorns{
         //loop through unicorns
         for(int i = 0; i < numUnicorns; i++){
 
-            Unicorn unicornValues = map.get(i);
-            double xValue = unicornValues.xCoordinate;
-            double uniDollars = unicornValues.uniDollars;
+            Unicorn realUnicorn = unicornList.get(i);
 
 
-            System.out.printf("%" + unicornWidth + "d" + "%" + 20 + "f" + "%" + 16 +"f", i, xValue, uniDollars);
+
+            //Unicorn unicornValues = xSortedMap.get(i);
+            double xValue = realUnicorn.xCoordinate;
+            double uniDollars = realUnicorn.uniDollars;
+
+
+            System.out.printf("%" + unicornWidth + "d" + "%" + 20 + ".1f" + "%" + 16 +".1f", i, xValue, uniDollars);
             System.out.println();
         
         }
